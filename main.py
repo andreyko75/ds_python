ffaf0a0d7c3119331dce9fae707c79795fba9485
Andrey <andreyko75@gmail.com>
2022-10-02 15:53:38 +0300

    py4

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# Допишите функцию check_user так, чтобы она по логину пользователя
-# проверяла, существует он или нет,
-# после чего с помощью вложенного условия проверяла
-# правильность пароля этого пользователя.
-# Функция должна возвращать только True или False.
-# Чтобы вернуть True, напишите "return True";
-# чтобы вернуть False, напишите "return False".
-
-user_database = {
-    'user': 'password',
-    'iseedeadpeople': 'greedisgood',
-    'hesoyam': 'tgm'
-}
-
-
-def check_user(username, password):
-    if username in user_database.keys() and password in user_database[username]:
-        return True
-    return False
+text = '''
+She sells sea shells on the sea shore;
+The shells that she sells are sea shells I am sure.
+So if she sells sea shells on the sea shore,
+I am sure that the shells are sea shore shells.
+'''
+text = text.lower()  # приводим текст к нижнему регистру
+text = text.replace(" ", "")  # заменяем пробелы на пустые строки
+text = text.replace("\n", "")  # заменяем символы переноса строки на пустые строки
+count_dict = {}  # создаём пустой словарь для подсчёта количества символов
+# создаём цикл по символам в строке text
+for symbol in text:  # symbol — текущий символ в тексте
+    # проверяем условие, что символа ещё нет среди ключей словаря
+    if symbol not in count_dict:  # если условие выполняется,
+        count_dict[symbol] = 1  # заносим символ в словарь со значением 1
+    else:  # в противном случае
+        count_dict[symbol] += 1  # увеличиваем частоту символа
+print(count_dict)  # выводим результирующий словарь
